73d6e91da08043020068deb8e8e68432c0aca60b
Yury Leshchuk <yuri.leshchuk@playrix.com>
2025-12-26 18:47:36 +0400

    taks for sprint 2

---
+++ task_5.py
@@ -0,0 +1,44 @@
+class Results:
+    def __init__(self, victories, draws, losses):
+        self.victories = victories
+        self.draws = draws
+        self.losses = losses
+
+
+class Football (Results):
+    def number_of_wins(self):
+        return f'Футбольных побед: {self.victories}'
+
+    def number_of_draws(self):
+        return f'Футбольных ничьих: {self.draws}'
+
+    def number_of_losses(self):
+        return f'Футбольных поражений: {self.losses}'
+
+    def total_points(self):
+        total = 3*self.victories + self.draws
+        return f'Общее количество очков: {total}'
+
+class Hockey (Results):
+    def number_of_wins(self):
+        return f'Хоккейных побед: {self.victories}'
+
+    def number_of_draws(self):
+        return f'Хоккейных ничьих: {self.draws}'
+
+    def number_of_losses(self):
+        return f'Хоккейных поражений: {self.losses}'
+
+    def total_points(self):
+        total = 2*self.victories + self.draws
+        return f'Общее количество очков: {total}'
+
+
+football_team = Football(2,2,2)
+hockey_team = Hockey(2,2,2)
+
+for team in (football_team, hockey_team):
+    print(team.number_of_wins())
+    print(team.number_of_draws())
+    print(team.number_of_losses())
+    print(team.total_points())
